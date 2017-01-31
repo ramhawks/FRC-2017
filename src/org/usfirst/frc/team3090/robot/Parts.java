df02402075781b1062810f37ddcade4e89bf08b2
Winona Ramhawks <winonaramhawks@gmail.com>
2017-01-30 18:45:29 -0600

    Added CAN Talons

---
+++ src/org/usfirst/frc/team3090/robot/Parts.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team3090.robot;
+
+import com.ctre.CANTalon;
+
+public class Parts {
+	public static final CANTalon front_left = new CANTalon(10);
+	public static final CANTalon back_left = new CANTalon(11);
+	public static final CANTalon front_right= new CANTalon(12);
+	public static final CANTalon back_right = new CANTalon(13);
+}
