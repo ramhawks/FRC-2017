7eea61cf85ddb09d4827d76bbba2510c762be250
Winona Ramhawks <winonaramhawks@gmail.com>
2017-02-09 17:54:24 -0600

    Add path name

--- src/org/usfirst/frc/team3090/robot/Path.java
+++ src/org/usfirst/frc/team3090/robot/Path.java
@@ -1,16 +1,18 @@
 package org.usfirst.frc.team3090.robot;
 
 public enum Path {
-	FIRST(
-		new Distance(24),
-		new Rotation(90),
-		new Distance(24)
-	);
-	
-	public Step[] steps;
-	
-	private Path(Step ... steps) {
+	FIRST("First", 
+			new Distance(24),
+			new Rotation(90),
+			new Distance(24));
+
+	public final String name;
+
+	public final Step[] steps;
+
+	private Path(String name, Step... steps) {
+		this.name = name;
 		this.steps = steps;
 	}
-	
+
 }
