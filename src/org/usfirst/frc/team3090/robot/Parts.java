e2bfe11b4b23fb0aabd71ef2164f10d03960f533
Winona Ramhawks <winonaramhawks@gmail.com>
2017-02-08 18:59:54 -0600

    Add lift talons

--- src/org/usfirst/frc/team3090/robot/Parts.java
+++ src/org/usfirst/frc/team3090/robot/Parts.java
@@ -7,9 +7,15 @@ public class Parts {
 	public static final int D2 = 12;
 	public static final int D3 = 13;
 	public static final int D4 = 14;
+	
+	public static final int L1 = 10;
+	public static final int L2 = 11;
 		
 	public static final CANTalon front_left = new CANTalon(D1);
 	public static final CANTalon back_left = new CANTalon(D2);
 	public static final CANTalon front_right= new CANTalon(D4);
 	public static final CANTalon back_right = new CANTalon(D3);
+	
+	public static final CANTalon lift_1 = new CANTalon(L1);
+	public static final CANTalon lift_2 = new CANTalon(L2);
 }
