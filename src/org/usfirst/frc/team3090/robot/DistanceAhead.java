d85c365084e67fd011cd7eb0c2d1bba7a3c793da
Winona Ramhawks <winonaramhawks@gmail.com>
2017-02-18 11:53:07 -0600

    Rename

--- src/org/usfirst/frc/team3090/robot/DistanceAhead.java
+++ src/org/usfirst/frc/team3090/robot/DistanceAhead.java
@@ -2,11 +2,11 @@ package org.usfirst.frc.team3090.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 
-public class DistanceForward extends Step {
+public class DistanceAhead extends Step {
 	
 	public final double meters;
 	
-	public DistanceForward(double meters) {
+	public DistanceAhead(double meters) {
 		if (meters < 0.3) {
 			DriverStation.reportWarning("One of the autonomous steps is a distance fewer than 30 cm, which is too short", false);
 		}
