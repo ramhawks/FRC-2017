f0a5531edfb57603754c2cb4b4959e43b6ba3123
Winona Ramhawks <winonaramhawks@gmail.com>
2017-04-13 09:17:00 -0500

    Create new auto paths for boilers

--- src/org/usfirst/frc/team3090/robot/Path.java
+++ src/org/usfirst/frc/team3090/robot/Path.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team3090.robot;
 
 public enum Path {
-	TEST("Test",
+/*	TEST("Test",
 			new DistanceBehind(1),
 			new Rotation(90),
 			new DistanceBehind(1)),
@@ -10,18 +10,30 @@ public enum Path {
 			new Rotation(60),
 			new DistanceAhead(0.4)),
 	MIDDLE("Middle",
-			new DistanceAhead(0.4)),
-	RIGHT("Right",
+			new DistanceAhead(0.4)),*/
+	/*RIGHT("Right",
 /*			new DistanceBehind(2.5),
 			new Rotation(-60),
-			new DistanceAhead(0.4)),*/
+			new DistanceAhead(0.4)),
 			new ForwardSecond(Robot.getTimeForDistance(2.5)),
 			new Rotation(-60),
-			new ForwardSecond(Robot.getTimeForDistance(.5))),
+			new ForwardSecond(Robot.getTimeForDistance(.5))),*/
 	SIXTY("Sixty",
 			new Rotation(60)),
-	FIVESEC("5 sec",
-			new ForwardSecond(5000));
+	FIVESEC("3 sec",
+			new ForwardSecond(3000)),
+	BOILER_RIGHT("Boiler on Right",
+			new Rotation(45),
+			new BackwardSecond(1),
+			new ShootSecond(3),
+			new Rotation(-45),
+			new BackwardSecond(1)),
+	BOILER_LEFT("Boiler on Left",
+			new Rotation(-45),
+			new ForwardSecond(1),
+			new ShootSecond(3),
+			new Rotation(45),
+			new ForwardSecond(1));
 
 	public final String name;
 
