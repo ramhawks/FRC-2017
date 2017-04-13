ae0119527348844a1ba88d397a3e5349796beef0
Winona Ramhawks <winonaramhawks@gmail.com>
2017-04-13 09:16:25 -0500

    Add new steps for path instructions

---
+++ src/org/usfirst/frc/team3090/robot/BackwardSecond.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3090.robot;
+
+public class BackwardSecond extends Step{
+	public final long seconds;
+	
+	public BackwardSecond(long seconds) {
+		this.seconds = seconds;
+	}
+}
